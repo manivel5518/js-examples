d6efef927916a2c76f077a88a92cb9405f17e2f5
manivel5518 <mvel589@gmail.com>
2022-09-15 18:55:23 +0530

    negative or positive

---
+++ negorpos.js
@@ -0,0 +1,15 @@
+let number=0;
+if(number>0)
+{
+console.log("the given number is positive");
+}
+else if (number<0)
+{
+	console.log("the given number is negative");
+}
+else
+{
+	console.log("the given number is zero");
+}
+
+ 
