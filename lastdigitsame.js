b18bf179702b5b5b4d3a8211b903a58382238078
manivel5518 <mvel589@gmail.com>
2022-09-16 14:03:05 +0530

    last digit same

---
+++ lastdigitsame.js
@@ -0,0 +1,14 @@
+const a=88;
+const n=48;
+const r=58;
+let res1= a % 10;
+let res2= n % 10;
+let res3= r % 10;
+if(res1==res2 && res1==res3)
+{
+	console.log("last digit are same");
+}
+else 
+{
+	console.log("last digit are not same");
+}
