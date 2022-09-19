aeada0081fca562c3187ed1fcd72e8c4b7464600
manivel5518 <mvel589@gmail.com>
2022-09-19 15:07:42 +0530

    string start with another string

---
+++ strstartwithanother.js
@@ -0,0 +1,8 @@
+const string="hello world";
+const check="he";
+if (string.startsWith(check))
+{
+	console.log("the string start with'he'.")
+}
+else
+console.log("the string not start with 'he'.");
