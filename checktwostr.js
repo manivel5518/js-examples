757ec7959961717c4238a812315c67dac2edfb3c
manivel5518 <mvel589@gmail.com>
2022-09-19 16:07:39 +0530

    check two string

---
+++ checktwostr.js
@@ -0,0 +1,12 @@
+const str1="hellow world";
+const str2="Hellow world";
+result=str1.toUpperCase()===str2.toUpperCase();
+if(result)
+{
+	console.log("string are equal");
+
+}
+else
+{
+	console.log("string are not equal");
+}
