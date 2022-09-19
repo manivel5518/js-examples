a6420eac9f869f1fbc5657d2dd58dc4d14e9b720
manivel5518 <mvel589@gmail.com>
2022-09-19 15:54:52 +0530

    check inside substring

---
+++ checksubstr.js
@@ -0,0 +1,10 @@
+const string="manivel";
+const substr="mani";
+if(string.includes(substr))
+{
+	console.log(`string contain substr ${substr}`);
+}
+else 
+{
+	console.log("string not contain substr");
+}
