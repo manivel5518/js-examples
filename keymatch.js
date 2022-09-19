9e7d83effc9316be17eec1436ea9af473f4638ce
manivel5518 <mvel589@gmail.com>
2022-09-19 11:11:16 +0530

    key check in onject

---
+++ keymatch.js
@@ -0,0 +1,10 @@
+const person={id:1,name:'mani',town:'chennai'}
+let haskey='name1' in person;
+if(haskey)
+{
+	console.log("key are existing");
+}
+else
+{
+	console.log("key are not existing");
+}
