63a11bea7758d97b2b43e64a1145259f3e846b0e
manivel5518 <mvel589@gmail.com>
2022-09-20 16:00:03 +0530

    check specified array

---
+++ checkspecival.js
@@ -0,0 +1,10 @@
+const arry=["mani","vel","nithil","chinna"];
+const arry1=arry.includes("mani");
+if(arry1)
+{
+	console.log("the array is include");
+}
+else
+{
+	console.log("the array is not include");
+}
