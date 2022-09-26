2831fdbabec4e900ebfcd3dc6acb66e4993b09b2
manivel5518 <mvel589@gmail.com>
2022-09-26 18:15:47 +0530

    check undefined or null

---
+++ checkundefinedornull.js
@@ -0,0 +1,16 @@
+function myfun(variable)
+{
+	if(variable==null)
+	{
+	console.log("the variable undefined or null");
+
+	}else
+	{
+	console.log("the varianle is defined nor null ");
+	}
+}
+let newvar;
+myfun(5);
+myfun('hellow');
+myfun(null);
+myfun(newvar);
